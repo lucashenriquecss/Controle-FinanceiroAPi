8a6eed00a6caa3b6724ed5040fdb96fe98db839e
Lucas Henrique Calheiros Saraiva Santos <43302698+lucashenriquecss@users.noreply.github.com>
2022-10-20 10:05:45 -0300

    Validação de despesas e receitas

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # Controle-FinanceiroAPi
- 
+## TASKS:
+- [&check;] Primera semana
+- [ ] Segunda semana
+- [ ] Terceira e Quarta semana
+## Validações:
+- [&check;] A API não deve permitir o cadastro de receitas e despesas duplicadas(contendo a mesma descrição, dentro do mesmo mês)
+- [ ]
+- [ ]
+- [ ]
+- [ ]
+- [ ]
+- [ ]
--- financeiro/serializers.py
+++ financeiro/serializers.py
@@ -1,6 +1,6 @@
 from rest_framework import serializers
 from financeiro.models import Despesas, Receitas
-
+import datetime
 
 
 class ReceitasSerializer(serializers.ModelSerializer):
@@ -9,10 +9,17 @@ class ReceitasSerializer(serializers.ModelSerializer):
         fields = '__all__'
 
     def validate_descricao(self,descricao):
+        x = datetime.datetime.now()
+
         if len(descricao) <= 2:
             raise serializers.ValidationError({'descricao':"A descrição deve ter mais de 2 letras"})
+        for instance in Receitas.objects.all():
+            if instance.data.strftime("%B") == x.strftime("%B") and instance.descricao == descricao: 
+                raise serializers.ValidationError({'data':"A descrição ja existe este mes"})
         return descricao
 
+    
+        
 
 
 
@@ -20,4 +27,15 @@ class ReceitasSerializer(serializers.ModelSerializer):
 class DespesasSerializer(serializers.ModelSerializer):
     class Meta:
         model = Despesas
-        fields = '__all__'    
+        fields = '__all__'
+
+    def validate_descricao(self,descricao):
+        x = datetime.datetime.now()
+
+        if len(descricao) <= 2:
+            raise serializers.ValidationError({'descricao':"A descrição deve ter mais de 2 letras"})
+        for instance in Despesas.objects.all():
+            if instance.data.strftime("%B") == x.strftime("%B") and instance.descricao == descricao: 
+                print(instance.data.strftime("%B"))
+                raise serializers.ValidationError({'data':"A descrição ja existe este mes"})
+        return descricao    
